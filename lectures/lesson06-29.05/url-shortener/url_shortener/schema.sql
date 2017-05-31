d54a68b66ae0369f86667a5d2be1f23e9294fc1a
ubilby <ubilby@mail.ru>
2017-05-31 18:49:55 +0300

    переименовал папку)Э

--- lectures/lesson06-29.05/url-shortener/url_shortener/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXIST shortener(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    original_url TEXT NOT NULL,
-    short_url TEXT NOT NULL DEFAULT "",
-    created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,               
-    )
